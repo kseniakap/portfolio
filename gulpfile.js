fd662c25fe1c79098bf79fe7b4e56d25c5f1816f
Ксения Капурина <kseniakap096@gmail.com>
2025-01-20 22:59:14 +0300

    Исправлен gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -10,82 +10,80 @@ const ghPages = require("gulp-gh-pages");
 
 // Static server
 gulp.task("server", function () {
-  browserSync.init({
-    server: {
-      baseDir: "dist",
-    },
-  });
-  gulp.watch("src/*.html").on("change", browserSync.reload);
+    browserSync.init({
+        server: {
+            baseDir: "dist",
+        },
+    });
+    gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
 gulp.task("styles", function () {
-  return gulp
-    .src("src/sass/**/*.+(scss|sass)")
-    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-    .pipe(
-      rename({
-        prefix: "",
-        suffix: ".min",
-      })
-    )
-    .pipe(
-      autoprefixer({
-        browsers: ["last 2 versions"],
-        cascade: false,
-      })
-    )
-    .pipe(cleanCSS({ compatibility: "ie8" }))
-    .pipe(gulp.dest("dist/css"))
-    .pipe(gulp.dest("src/css"))
-    .pipe(browserSync.stream());
+    return gulp
+        .src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+        .pipe(
+            rename({
+                prefix: "",
+                suffix: ".min",
+            })
+        )
+        .pipe(
+            autoprefixer({
+                browsers: ["last 2 versions"],
+                cascade: false,
+            })
+        )
+        .pipe(cleanCSS({ compatibility: "ie8" }))
+        .pipe(gulp.dest("dist/css"))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
 });
 
 gulp.task("watch", function () {
-  gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel("styles"));
-  // gulp.watch("src/*.html").on("change", browserSync.reload);
-  gulp.watch("src/*.html").on("change", gulp.parallel("html"));
-  gulp.watch("src/js/**/*.js").on("change", gulp.parallel("scripts"));
-  gulp.watch("src/fonts/**/*").on("all", gulp.parallel("fonts"));
-  gulp.watch("src/icons/**/*").on("add", gulp.parallel("icons"));
-  gulp.watch("src/img/**/*").on("add", gulp.parallel("images"));
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel("styles"));
+    gulp.watch("src/*.html").on("change", gulp.parallel("html"));
+    gulp.watch("src/js/**/*.js").on("change", gulp.parallel("scripts"));
+    gulp.watch("src/fonts/**/*").on("all", gulp.parallel("fonts"));
+    gulp.watch("src/icons/**/*").on("add", gulp.parallel("icons"));
+    gulp.watch("src/img/**/*").on("add", gulp.parallel("images"));
 });
 
 gulp.task("html", function () {
-  return gulp
-    .src("src/*.html")
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest("dist/"));
+    return gulp
+        .src("src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/"));
 });
 
 gulp.task("scripts", function () {
-  return gulp
-    .src("src/js/**/*.js")
-    .pipe(gulp.dest("dist/js"))
-    .pipe(browserSync.stream());
+    return gulp
+        .src("src/js/**/*.js")
+        .pipe(gulp.dest("dist/js"))
+        .pipe(browserSync.stream());
 });
 
 gulp.task("fonts", function () {
-  return gulp
-    .src("src/fonts/**/*")
-    .pipe(gulp.dest("dist/fonts"))
-    .pipe(browserSync.stream());
+    return gulp
+        .src("src/fonts/**/*")
+        .pipe(gulp.dest("dist/fonts"))
+        .pipe(browserSync.stream());
 });
 
 gulp.task("icons", function () {
-  return gulp
-    .src("src/icons/**/*")
-    .pipe(gulp.dest("dist/icons"))
-    .pipe(browserSync.stream());
+    return gulp
+        .src("src/icons/**/*")
+        .pipe(gulp.dest("dist/icons"))
+        .pipe(browserSync.stream());
 });
 
 gulp.task("images", function () {
-  return gulp
-    .src("src/img/**/*")
-    .pipe(imagemin())
-    .pipe(gulp.dest("dist/img"))
-    .pipe(browserSync.stream());
+    return gulp
+        .src("src/img/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/img"))
+        .pipe(browserSync.stream());
 });
-
 
 gulp.task(
     "default",
@@ -100,8 +98,8 @@ gulp.task(
         "images"
     )
 );
-
-gulp.task("deploy", function () {
+// Исправленный deploy task
+gulp.task("deploy", function() {
     return gulp.src("./dist/**/*")
         .pipe(ghPages({
             branch: 'gh-pages',
